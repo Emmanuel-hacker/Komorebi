59aaf9362426b028d7432193aa29079b72939941
kenrag24 <kenrag24@gmail.com>
2020-06-26 22:45:19 -0500

    Added inital stuff

---
+++ src/bot.js
@@ -0,0 +1,25 @@
+const { AkairoClient, CommandHandler, ListenerHandler } = require('discord-akairo')
+const { token, prefix } = require('../config.json')
+class Client extends AkairoClient {
+    constructor() {
+        super({
+            ownerID: '467030554131562506'
+        }, {
+            disableMentions: 'everyone'
+        })
+
+        this.commandHandler = new CommandHandler(this, {
+            directory: './src/commands/',
+            prefix: prefix
+        });
+
+        this.listenerHandler = new ListenerHandler(this, {
+            directory: './src/listeners/'
+        });
+        this.commandHandler.loadAll();
+        this.listenerHandler.loadAll()
+    }
+}
+
+const client = new Client()
+client.login(token)
