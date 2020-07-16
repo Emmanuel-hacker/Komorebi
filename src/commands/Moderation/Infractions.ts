678829e00e0f597c7fa3b94348489915d656d2f5
kenrag24 <kenrag24@gmail.com>
2020-07-16 10:49:04 -0500

    added infractions command

---
+++ src/commands/Moderation/Infractions.ts
@@ -0,0 +1,53 @@
+import { Command } from "discord-akairo"
+import { Message, GuildMember, MessageEmbed } from "discord.js"
+import { Repository } from "typeorm"
+
+import { warns } from "../../models/Warns"
+import { User } from "discord.js"
+
+export default class InfractionsCommand extends Command {
+    public constructor() {
+        super('infractions', {
+            aliases: ['infractions', 'warns'],
+            category: 'Moderation',
+            description: {
+                content: 'view a members warns',
+                usage: 'infractions [ member ]'
+            },
+            userPermissions: ['MANAGE_MESSAGES'],
+
+            args: [
+                {
+                    id: "member",
+                    type: "member",
+                    default: (msg: Message) => msg.member
+                }
+            ]
+        })  
+    }
+
+    public async exec(message: Message, { member }: {member: GuildMember}): Promise<Message> {
+        const warnRepo: Repository<warns> = this.client.db.getRepository(warns)
+        const Warns: warns[] = await warnRepo.find({user: member.id, guild: message.guild.id})
+
+        if(!Warns.length) return message.util.send(`No infractions found`)
+
+        const infractions = await Promise.all(Warns.map(async (v: warns, i: number)=> {
+            const mod: User = await this.client.users.fetch(v.moderator).catch(() => null)
+            if(mod) return {
+                index: i + 1,
+                moderator: mod.tag,
+                reason: v.reason
+            }
+        }))
+
+        return message.util.send(new MessageEmbed()
+        .setAuthor(`Infractions | ${member.user.username}`, member.user.displayAvatarURL())
+        .setColor("RANDOM")
+        .setDescription(infractions.map(v => `\`#${v.index}\` | Moderator: **${v.moderator}**\nReason: *${v.reason}*`))
+
+        
+        )
+        
+    }
+}
